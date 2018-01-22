8132a08071fcae6371cc95417e136d4454f032b8
Remy Bertot <remy@passbolt.com>
2018-01-22 13:33:46 +0530

    Move _site to docs folder

---
+++ docs/assets/js/search.js
@@ -0,0 +1,69 @@
+/**
+ * Search
+ *
+ * @licence MIT License
+ * @copyright Copyright (c) 2017 CloudCannon
+ * @copyright Copyright (c) 2018 Passbolt SARL
+ */
+(function() {
+  function displaySearchResults(results, store) {
+    var searchResults = document.getElementById('search-results');
+
+    if (results.length) { // Are there any results?
+      var appendString = '';
+
+      for (var i = 0; i < results.length; i++) {  // Iterate over the results
+        var item = store[results[i].ref];
+        appendString += '<li>';
+        appendString += '<h2><a href="' + item.url + '">' + item.title + '</a></h2>';
+        appendString += '<a class="url" href="' + item.url + '">' + item.url + '</a>';
+        appendString += '<p>' + item.content.substring(0, 150).trim() + '...</p>'
+        appendString += '</li>';
+      }
+
+      searchResults.innerHTML = appendString;
+    }
+  }
+
+  function getQueryVariable(variable) {
+    var query = window.location.search.substring(1);
+    var vars = query.split('&');
+
+    for (var i = 0; i < vars.length; i++) {
+      var pair = vars[i].split('=');
+
+      if (pair[0] === variable) {
+        return decodeURIComponent(pair[1].replace(/\+/g, '%20'));
+      }
+    }
+  }
+
+  var searchTerm = getQueryVariable('query');
+
+  if (searchTerm) {
+    document.getElementById('search-box').setAttribute("value", searchTerm);
+
+    // Initalize lunr with the fields it will be searching on. I've given title
+    // a boost of 10 to indicate matches on this field are more important.
+    var idx = lunr(function () {
+      this.field('id');
+      this.field('title', { boost: 10 });
+      this.field('category', { boost: 2 });
+      this.field('content');
+    });
+
+
+
+    for (var key in window.store) { // Add the data to lunr
+      idx.add({
+        'id': key,
+        'title': window.store[key].title,
+        'category': window.store[key].category,
+        'content': window.store[key].content
+      });
+
+      var results = idx.search(searchTerm); // Get lunr to perform a search
+      displaySearchResults(results, window.store); // We'll write this in the next section
+    }
+  }
+})();

06da6ea2fc4c52c4bc30ccaeed00c235288efa34
Kevin Muller <kevin@passbolt.com>
2022-03-03 15:37:34 +0100

    PB-9710 Implement cookiebot and matomo on help site

---
+++ assets/js/matomo-tag-manager.js
@@ -0,0 +1,6 @@
+<!-- Matomo Tag Manager -->
+var _mtm = window._mtm = window._mtm || [];
+_mtm.push({'mtm.startTime': (new Date().getTime()), 'event': 'mtm.Start'});
+var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+g.async=true; g.src='https://cdn.matomo.cloud/passbolt.matomo.cloud/container_C0l5nxM2.js'; s.parentNode.insertBefore(g,s);
+<!-- End Matomo Tag Manager -->
--- docs/assets/js/analytics.js
+++ docs/assets/js/analytics.js
@@ -1,10 +1,18 @@
-$(document).ready(() => {
-  new CookiesEuBanner(function () {
-    window.dataLayer = window.dataLayer || [];
-    function gtag() {
-      dataLayer.push(arguments);
-    }
-    gtag('js', new Date());
-    gtag('config', 'UA-23506466-1', {'allowAdFeatures': false, 'anonymizeIp': true});
-  }, true);
-});
+function CookiebotCallback_OnAccept() {
+  if (Cookiebot.consent.statistics) {
+    enableStatisticsCookies();
+  } else {
+    disableStatisticsCookies();
+  }
+}
+function CookiebotCallback_OnDecline() {
+  if (!Cookiebot.consent.statistics) {
+    disableStatisticsCookies();
+  }
+}
+function enableStatisticsCookies() {
+  _paq.push(['setCookieConsentGiven']);
+}
+function disableStatisticsCookies() {
+  _paq.push(['forgetCookieConsentGiven']);
+}
